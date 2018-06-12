bd428d1be5f770950b5103d80cb04d3f91b90ed1
Kieran Williams <kieranw@wix.com>
2018-06-12 11:29:50 +0300

    Changed types

--- packages/meta-data-tools/src/types.ts
+++ packages/meta-data-tools/src/types.ts
@@ -1,31 +1,35 @@
 import { ComponentType } from 'react';
 
-export interface IRegistry {
+export interface IRegistry<AssetMap> {
   metadata: IMetadata;
   describeComponent:<Props> (comp: ComponentType<Props>) => IComponentMetadata<Props>;
-  describeAsset: (asset: any) => IAssetMetadata;
+  describeAsset:<AssetType extends keyof AssetMap, Asset extends AssetMap[AssetType]> (asset: Asset, type: AssetType, name: string, description?: string) => IAssetMetadata<AssetType>;
+  describeTheme: (style: any) => IStyleMetadata;
   clean: () => void;
 }
 
 export interface IComponentMetadata<Props> {
   simulations: Simulation<Props>[];
-  styles: Map<any, IAssetMetadata>;
+  styles: Map<any, IStyleMetadata>;
   addSim: (sim: Simulation<Props>) => void;
-  
   addStyle: (style: any) => void;
 }
 
 export interface IMetadata {
   components: Map<ComponentType<any>, IComponentMetadata<any>>;
-  assets: Map<any, IAssetMetadata>;
+  assets: Map<any, IAssetMetadata<any>>;
+  themes: Map<any, IStyleMetadata>;
 }
 
-export interface IAssetMetadata {
-  path: string;
+export interface IAssetMetadata<AssetType> {
+  type: AssetType;
   name: string;
-  exports: string[];
+  description?: string;
 }
 
+export interface IStyleMetadata {
+  name: string;
+}
 export interface Simulation<Props> {
   props: Props;
 }

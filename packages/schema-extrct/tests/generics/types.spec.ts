bcfc337beeed445ea5f3b0da5b7891dd377610bc
Nadav Abrahami <nadav@lan.wixpress.com>
2018-06-14 13:22:06 +0300

    added tests for generic schemas

---
+++ packages/schema-extrct/tests/generics/types.spec.ts
@@ -0,0 +1,171 @@
+import {expect} from 'chai';
+import { ModuleSchema, FunctionSchemaId } from '../../src/json-schema-types';
+import {transformTest} from '../../test-kit/run-transform'
+
+
+
+describe('schema-extrct - generic types',()=>{
+    it('should support genric type definition', async ()=>{
+        const moduleId = '/ui-autotools/type-definition';
+        const res = transformTest(`
+        export type MyType<T> = {
+            something:T;
+        };
+        export let param:MyType<string>;
+        `, moduleId);
+
+        const expected:ModuleSchema<'object'> = {
+            "$schema": "http://json-schema.org/draft-06/schema#",
+            "$id":moduleId,
+            "$ref":"common/module",
+            "definitions":{
+                "MyType" : {
+                    "type":"object",
+                    "genericParams": [{
+                        name:"T"
+                    }],
+                    "properties": {
+                        "something":{
+                            "$ref":"#MyType!T"
+                        } 
+                    }
+                }
+            },
+            "properties": {
+                "param":{
+                    "$ref":"#MyType",
+                    "genericArguments":[{
+                        "type":"string"
+                    }]
+                }
+            }
+        }
+        expect(res).to.eql(expected);
+    });
+
+    it('should support generic arguments schema', async ()=>{
+        const moduleId = '/ui-autotools/type-definition';
+        const res = transformTest(`
+        export type MyType<T extends string> = {
+            something:T;
+        };
+        export let param:MyType<'gaga'>;
+        `, moduleId);
+
+        const expected:ModuleSchema<'object'> = {
+            "$schema": "http://json-schema.org/draft-06/schema#",
+            "$id":moduleId,
+            "$ref":"common/module",
+            "definitions":{
+                "MyType" : {
+                    "type":"object",
+                    "genericParams": [{
+                        name:"T",
+                        "type":"string"
+                    }],
+                    "properties": {
+                        "something":{
+                            "$ref":"#MyType!T"
+                        } 
+                    }
+                }
+            },
+            "properties": {
+                "param":{
+                    "$ref":"#MyType",
+                    "genericArguments":[{
+                        "type":"string",
+                        "enum":[
+                            "gaga"
+                        ]
+                    }]
+                }
+            }
+        }
+        expect(res).to.eql(expected);
+    });
+
+
+    it('generic arguments should be passed deeply', async ()=>{
+        const moduleId = '/ui-autotools/type-definition';
+        const res = transformTest(`
+        export type MyType<T extends string> = {
+            something:{
+                deepKey:T
+            };
+            method:(arg:{
+                values:T[],
+                filter:(item:T)=>boolean
+            })=>{
+                status:string,
+                results:T[]
+            }
+        };
+        `, moduleId);
+
+        const expected:ModuleSchema<'object'> = {
+            "$schema": "http://json-schema.org/draft-06/schema#",
+            "$id":moduleId,
+            "$ref":"common/module",
+            "definitions":{
+                "MyType" : {
+                    "type":"object",
+                    "genericParams": [{
+                        name:"T",
+                        "type":"string"
+                    }],
+                    "properties": {
+                        "something":{
+                            "type":"object",
+                            "properties":{
+                                "deepKey":{
+                                    "$ref":"#MyType!T"
+                                }
+                            }
+                        },
+                        "method":{
+                            "$ref":FunctionSchemaId,
+                            "arguments":[
+                                {
+                                    "name":"values",
+                                    "type":"array",
+                                    "items":{
+                                        "$ref":"#MyType!T"
+                                    }
+                                },{
+                                    "name":"filter",
+                                    "$ref":FunctionSchemaId,
+                                    "arguments":[
+                                        {
+                                            "name":"item",
+                                            "$ref":"#MyType!T"
+                                        }
+                                    ],
+                                    "returns":{
+                                        "type":"boolean"
+                                    }
+                                }
+                            ],
+                            "returns":{
+                                "type":"object",
+                                "properties":{
+                                    "status":{
+                                        "type":"string"
+                                    },
+                                    "results":{  
+                                        "type":"array",
+                                        "items":{
+                                            "$ref":"#MyType!T"
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        expect(res).to.eql(expected);
+    });
+})
+

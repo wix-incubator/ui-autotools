551cf6a944d111415d39be1c281620bfb0dde17e
Kieran Williams <kieranw@wix.com>
2018-06-25 16:49:28 +0300

    Added helpful error message

--- packages/metadata-tools/src/registry/registry.ts
+++ packages/metadata-tools/src/registry/registry.ts
@@ -14,6 +14,8 @@ const Registry: IRegistry<IAssetMap> = {
   describeComponent <Props>(comp: ComponentType<Props>): ComponentMetadata<Props> {
     if (!this.metadata.components.has(comp)) {
       this.metadata.components.set(comp, new ComponentMetadata<Props>());
+    } else {
+      throw new Error('Cannot re-describe a Component. Make sure that you are only trying to describe a Component once.');
     }
 
     return this.metadata.components.get(comp)!;
@@ -21,6 +23,8 @@ const Registry: IRegistry<IAssetMap> = {
   describeAsset <AssetType extends keyof IAssetMap, Asset extends IAssetMap[AssetType]>(asset: Asset, type: AssetType, name: string, description?: string): IAssetMetadata {
     if (!this.metadata.assets.has(asset)) {
       this.metadata.assets.set(asset, new AssetMetadata(type, name, description));
+    } else {
+      throw new Error('Cannot re-describe an Asset. Make sure that you are only trying to describe an Asset once.');
     }
 
     return this.metadata.assets.get(asset)!;
@@ -28,6 +32,8 @@ const Registry: IRegistry<IAssetMap> = {
   describeTheme(theme: any, name: string): IThemeMetadata {
     if (!this.metadata.themes.has(theme)) {
       this.metadata.themes.set(theme, new ThemeMetadata(name));
+    } else {
+      throw new Error('Cannot re-describe a Theme. Make sure that you are only trying to describe a Theme once.');
     }
 
     return this.metadata.themes.get(theme)!;

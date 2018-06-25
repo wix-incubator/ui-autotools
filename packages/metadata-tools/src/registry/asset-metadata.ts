180f5bb1688a7798a5c24ff1d9f649b1ec1ea26f
Kieran Williams <kieranw@wix.com>
2018-06-25 16:46:16 +0300

    Refactored theme and asset metadata

--- packages/metadata-tools/src/registry/asset-metadata.ts
+++ packages/metadata-tools/src/registry/asset-metadata.ts
@@ -1,13 +1,5 @@
 import { IAssetMetadata } from './types';
 
 export default class AssetMetadata implements IAssetMetadata {
-  public type: string;
-  public name: string;
-  public description: string | undefined;
-
-  constructor(type: string, name: string, description?: string) {
-    this.type = type;
-    this.name = name;
-    this.description = description;
-  }
+  constructor(public type: string, public name: string, public description?: string) {}
 }
--- packages/metadata-tools/src/registry/theme-metadata.ts
+++ packages/metadata-tools/src/registry/theme-metadata.ts
@@ -1,9 +1,5 @@
 import { IThemeMetadata } from './types';
 
 export default class ThemeMetadata implements IThemeMetadata {
-  public name: string;
-
-  constructor(name: string) {
-    this.name = name;
-  }
+  constructor(public name: string) {}
 }

c7e1b068b325761426cd52606fe7507cee28f17e
Kieran Williams <kieranw@wix.com>
2018-06-25 13:41:48 +0300

    Fixed tests

--- packages/metadata-tools/test/metadata-tools.spec.tsx
+++ packages/metadata-tools/test/metadata-tools.spec.tsx
@@ -22,22 +22,22 @@ describe('Registry', () => {
   });
 
   it('returns an already existing metadata', () => {
-    const myCompMetaData = Registry.describe(TestComp);
-    const mySecondCompMetaData = Registry.describe(TestComp);
+    const myCompMetaData = Registry.describeComponent(TestComp);
+    const mySecondCompMetaData = Registry.describeComponent(TestComp);
 
     expect(mySecondCompMetaData).to.equal(myCompMetaData);
   });
 
-  describe('The Describe method', () => {
+  describe('The describeComponent method', () => {
     it('adds a new component\'s metadata to the registry, and returns its meta data', () => {
-      const myCompMetaData = Registry.describe(TestComp);
+      const myCompMetaData = Registry.describeComponent(TestComp);
       expect(myCompMetaData).to.be.an.instanceof(ComponentMetadata);
     });
   });
 
   describe('The addSim method', () => {
     it('adds a new simulation to the component metadata', () => {
-      const myCompMetaData = Registry.describe(TestComp);
+      const myCompMetaData = Registry.describeComponent(TestComp);
       myCompMetaData.addSim(testSim);
       expect(myCompMetaData.simulations[0]).to.equal(testSim);
     });
@@ -45,12 +45,12 @@ describe('Registry', () => {
 
   describe('The clean method', () => {
     it('removes any existing metadata', () => {
-      Registry.describe(TestComp);
-      expect(Registry.metadata.size).to.equal(1);
+      Registry.describeComponent(TestComp);
+      expect(Registry.metadata.components.size).to.equal(1);
 
       Registry.clean();
 
-      expect(Registry.metadata.size).to.equal(0);
+      expect(Registry.metadata.components.size).to.equal(0);
     });
   });
 });

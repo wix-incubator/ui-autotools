7e8c673b42500ad8d533317c8575eb704b214f65
Alexey Lebedev <alisey.login@gmail.com>
2018-06-26 15:37:11 +0300

    Add meta webpack config and some simulations

---
+++ packages/mock-repo/meta.webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const glob = require('glob');
+
+const packagePath = __dirname;
+const metaGlob = 'src/**/*.meta.ts?(x)';
+
+module.exports = {
+  context: packagePath,
+  mode: 'development',
+  entry: {
+    meta: glob.sync(path.join(packagePath, metaGlob)),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            compilerOptions: {
+              declaration: false
+            }
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json']
+  }
+};

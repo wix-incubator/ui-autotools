53c34970077b5a0c4309399cfd9b4d3a9e620faf
Alexey Lebedev <alisey.login@gmail.com>
2018-06-27 15:12:25 +0300

    Remove obsolete comment

--- packages/mock-repo/src/modal/modal.meta.ts
+++ packages/mock-repo/src/modal/modal.meta.ts
@@ -1,9 +1,6 @@
 import Registry from 'metadata-tools';
 import {Modal} from './modal';
 
-// [ISSUE] this component needs to be a child of another component, and we
-// have no ability to express this as a simulation.
-
 Registry.getComponentMetadata(Modal)
   .addSim({
     props: {

c2a8f16e7fe9a68e3f3b514713b5b51b91651914
Alexey Lebedev <alisey.login@gmail.com>
2018-06-28 17:22:06 +0300

    Helpers for starting a server and configuring webpack

---
+++ packages/eyes/src/index.ts
@@ -0,0 +1,45 @@
+/* tslint:disable:no-console */
+
+import path from 'path';
+import {WebpackConfigurator} from 'ui-autotools-utils';
+import {serve, IServer} from 'ui-autotools-utils';
+
+const packagePath = path.resolve(__dirname, '..');
+const projectPath = process.cwd();
+const webpackConfigPath = path.join(projectPath, 'meta.webpack.config.js');
+
+function getWebpackConfig() {
+  return WebpackConfigurator
+    .load(webpackConfigPath)
+    .addEntry('meta', path.join(packagePath, 'src/browser/run'))
+    .addHtml({
+      template: path.join(packagePath, 'src/browser/index.html'),
+      title: 'Eyes'
+    })
+    .suppressReactDevtoolsSuggestion()
+    .getConfig();
+}
+
+async function main() {
+  let server: IServer | null = null;
+  try {
+    server = await serve({webpackConfig: getWebpackConfig()});
+    const numFailedTests = await runTests(server.getUrl());
+    process.exitCode = numFailedTests ? 1 : 0;
+  } catch (error) {
+    process.exitCode = 1;
+    console.error(error ? error.message : '');
+  } finally {
+    if (server) {
+      server.close();
+    }
+    process.exit();
+  }
+}
+
+async function runTests(url: string) {
+  console.log(`Serving meta bundle on ${url}`);
+  return 0;
+}
+
+main();
---
+++ packages/utils/src/serve/log.ts
@@ -0,0 +1,73 @@
+import readline from 'readline';
+import chalk from 'chalk';
+import webpack from 'webpack';
+
+function clearConsole() {
+  process.stdout.write(
+    process.platform === 'win32' ?
+      '\x1B[2J\x1B[0f' :
+      '\x1B[2J\x1B[3J\x1B[H'
+  );
+}
+
+function formatWebpackStats(stats: webpack.Stats) {
+  return stats.toString({
+    assets: false,
+    builtAt: false,
+    chunks: false,
+    colors: true,
+    entrypoints: false,
+    hash: false,
+    modules: false,
+    performance: false,
+    timings: false,
+    version: false
+  });
+}
+
+export class Log {
+  private firstRun: boolean = true;
+  private listening: boolean = false;
+  private watch: boolean = false;
+
+  constructor(watch: boolean) {
+    this.watch = watch;
+  }
+
+  // Postpone progress logging until the server is listening to avoid mixing
+  // compiler's and server's output, since they start in parallel.
+  public compilationProgress = (percentage: number) => {
+    if (this.listening && this.watch) {
+      readline.clearLine(process.stdout, 0);
+      readline.cursorTo(process.stdout, 0);
+      if (percentage < 1) {
+        process.stdout.write(`Compiling ${Math.round(100 * percentage)}%`);
+      }
+    }
+  }
+
+  public compilationFinished = (stats: webpack.Stats) => {
+    if (this.firstRun) {
+      this.firstRun = false;
+    } else if (this.watch) {
+      clearConsole();
+    }
+
+    if (stats.hasErrors()) {
+      process.stderr.write(chalk.red('Compilation failed.\n'));
+      process.stderr.write(formatWebpackStats(stats));
+    } else if (stats.hasWarnings()) {
+      process.stderr.write(chalk.yellow('Compiled with warnings.\n'));
+      process.stderr.write(formatWebpackStats(stats));
+    } else if (this.watch) {
+      process.stdout.write(chalk.green('Compiled successfully.\n'));
+    }
+  }
+
+  public serverListening = (url: string) => {
+    this.listening = true;
+    if (this.watch) {
+      process.stdout.write(`Running on ${chalk.blue(url)}\n`);
+    }
+  }
+}

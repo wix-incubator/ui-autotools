b34826f40781721385a5da1c113c4a1989be3844
Alexey Lebedev <alisey.login@gmail.com>
2018-07-03 11:24:45 +0300

    Eyes: add cli and Puppeteer

--- packages/eyes/src/index.ts
+++ packages/eyes/src/index.ts
@@ -1,8 +1,8 @@
 /* tslint:disable:no-console */
 
 import path from 'path';
-import {WebpackConfigurator} from 'ui-autotools-utils';
-import {serve, IServer} from 'ui-autotools-utils';
+import puppeteer from 'puppeteer';
+import {WebpackConfigurator, serve, IServer} from 'ui-autotools-utils';
 
 const packagePath = path.resolve(__dirname, '..');
 const projectPath = process.cwd();
@@ -20,15 +20,99 @@ function getWebpackConfig() {
     .getConfig();
 }
 
-async function main() {
+async function runTests(url: string) {
+  const viewportWidth = 800;
+  const viewportHeight = 600;
+
+  let browser: puppeteer.Browser | null = null;
+  let numFailedTests: number | null = null;
+  try {
+    browser = await puppeteer.launch({headless: true});
+    const page = await browser.newPage();
+    await page.setViewport({width: viewportWidth, height: viewportHeight});
+
+    page.on('dialog', (dialog) => {
+      dialog.dismiss();
+    });
+
+    // TODO: move to utils
+    page.on('console', async (msg) => {
+      const args = await Promise.all(msg.args().map((a) => a.jsonValue()));
+      console.log(...args);
+    });
+
+    numFailedTests = await Promise.race([
+      waitForPageError(page),
+      waitForTestsCompletion(page, url)
+    ]);
+
+    await browser.close();
+
+    return numFailedTests;
+  } catch (error) {
+    if (browser) {
+      try {
+       browser!.close();
+      } catch (_) {
+        // Ignore the error since we're already handling an exception.
+      }
+    }
+
+    throw error;
+  }
+}
+
+// TODO: move to utils
+function waitForPageError(page: puppeteer.Page): Promise<never> {
+  return new Promise((_, reject) => {
+    page.on('pageerror', (errorText: string) => {
+      reject(new Error(errorText));
+    });
+
+    page.on('error', () => {
+      reject(new Error('Page crashed'));
+    });
+  });
+}
+
+async function waitForTestsCompletion(page: puppeteer.Page, url: string):
+  Promise<number> {
+  const loadTimeout = 20000;
+
+  const getTests = new Promise<Array<{title: string}>>((resolve) =>
+    page.exposeFunction('puppeteerRunTests', resolve)
+  );
+
+  await page.goto(url, {timeout: loadTimeout});
+
+  const tests = await getTests;
+
+  // TODO: add a timeout for each test to make sure we terminate if they get
+  // stuck.
+  for (const [i, {title}] of tests.entries()) {
+    await page.evaluate(`puppeteerRenderTest(${i})`);
+    const screenshot = await page.screenshot();
+    // TODO: Compare the screenshot using eyes
+    console.log({title, screenshotBufferSize: screenshot.byteLength});
+    await page.evaluate(`puppeteerCleanupTest(${i})`);
+  }
+
+  return 0;
+}
+
+export async function eyesTest() {
   let server: IServer | null = null;
   try {
     server = await serve({webpackConfig: getWebpackConfig()});
     const numFailedTests = await runTests(server.getUrl());
-    process.exitCode = numFailedTests ? 1 : 0;
+    if (numFailedTests) {
+      process.exitCode = 1;
+    }
   } catch (error) {
     process.exitCode = 1;
-    console.error(error ? error.message : '');
+    if (error) {
+      process.stderr.write(error.toString());
+    }
   } finally {
     if (server) {
       server.close();
@@ -36,10 +120,3 @@ async function main() {
     process.exit();
   }
 }
-
-async function runTests(url: string) {
-  console.log(`Serving meta bundle on ${url}`);
-  return 0;
-}
-
-main();

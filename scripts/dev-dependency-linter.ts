413a40c47fa1ec085993b39741e22fe2609f7fda
Kieran Williams <kieranw@wix.com>
2018-07-04 14:55:51 +0300

    Ignoring node_modules, made linter async, using error template with colours, and added comments.

--- scripts/dev-dependency-linter.ts
+++ scripts/dev-dependency-linter.ts
@@ -1,27 +1,63 @@
-const fs = require('fs');
-const glob = require('glob');
+import {readFile} from 'proper-fs';
+import glob from 'glob';
+import path from 'path';
 
-export const devDependencyLinter = (pathToPackageJson: string): string[] => {
-  const errors = [];
-  const {workspaces} = JSON.parse(fs.readFileSync(pathToPackageJson));
+type ErrorType = 'Error' | 'Warning';
+
+interface IPackageError {
+  type: ErrorType;
+  packageName: string;
+  message: string;
+}
+
+class PackageError implements IPackageError {
+  constructor(public type: ErrorType, public packageName: string, public message: string) {}
+}
+
+export async function devDependencyLinter(pathToPackageJson: string) {
+  const errors: PackageError[] = [];
+  let workspaces;
+
+  // Parse the root package.json and retrieve workspaces
+  try {
+    workspaces = JSON.parse((await readFile(pathToPackageJson)).toString()).workspaces;
+  } catch (e) {
+    errors.push(new PackageError('Error', pathToPackageJson, e));
+    return errors;
+  }
+
+  // Return an error if there aren't any workspaces
+  if (!workspaces || workspaces.length === 0) {
+    errors.push(new PackageError('Error', pathToPackageJson, `Couldn't find any valid workspaces.`));
+    return errors;
+  }
 
   for (const workspace of workspaces) {
-    const packages = glob.sync(workspace);
+    // Grab the paths to the package.json files in each package of the workspace
+    const packages = glob.sync(path.join(workspace, 'package.json'), {ignore: 'node_modules'});
+
+    // If there aren't any, return an error
     if (!packages || packages.length === 0) {
-      errors.push(`Couldn't find any packages in the workspace.`);
+      errors.push(new PackageError('Error', workspace, `Couldn't find any packages in the workspace.`));
       return errors;
     }
 
     for (const pkg of packages) {
-      const {devDependencies} = JSON.parse(fs.readFileSync(`${pkg}/package.json`));
+      const pathToPkg = path.resolve(pkg);
+      let devDependencies;
+
+      try {
+        devDependencies = JSON.parse((await readFile(pathToPkg)).toString()).devDependencies;
+      } catch (e) {
+        errors.push(new PackageError('Error', pathToPkg, e));
+        return errors;
+      }
 
       if (devDependencies) {
-        for (const devDependency of Object.keys(devDependencies)) {
-          errors.push(`Package "${pkg}" cannot have devDependency "${devDependency}" in its package.json. This dev-dependency should be placed in the root package.json`);
-        }
+        errors.push(new PackageError('Error', pathToPkg, `Package "${pkg}" cannot have devDependencies: "${Object.keys(devDependencies)}" in its package.json. This devDependency should be placed in the root package.json.`));
       }
     }
   }
 
   return errors;
-};
+}
--- scripts/json-linter.ts
+++ scripts/json-linter.ts
@@ -1,10 +1,15 @@
 import {devDependencyLinter} from './dev-dependency-linter';
 import {join} from 'path';
+import process from 'process';
+import chalk from 'chalk';
 
 const pathToPackageJson = join(__dirname, '../package.json');
 
-const errors = devDependencyLinter(pathToPackageJson);
-
-for (const error of errors) {
-  throw new Error(error);
-}
+devDependencyLinter(pathToPackageJson).then((errors) => {
+  for (const error of errors) {
+    const errorColour = error.type === 'Error' ? 'red' : 'yellow';
+    // tslint:disable-next-line:no-console
+    console.error(`${chalk[errorColour](error.type)} in ${chalk.underline(error.packageName)}: ${error.message}`);
+  }
+  process.exit(1);
+});

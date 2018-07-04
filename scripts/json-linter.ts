b3160e6d5b81d60caa70a27e9b3afbeb045c625e
Kieran Williams <kieranw@wix.com>
2018-07-04 15:40:33 +0300

    Added console.log to mark linting

--- scripts/json-linter.ts
+++ scripts/json-linter.ts
@@ -5,11 +5,15 @@ import chalk from 'chalk';
 
 const pathToPackageJson = join(__dirname, '../package.json');
 
+// tslint:disable-next-line:no-console
+console.log('Linting devDependencies.');
 devDependencyLinter(pathToPackageJson).then((errors) => {
-  for (const error of errors) {
-    const errorColour = error.type === 'Error' ? 'red' : 'yellow';
-    // tslint:disable-next-line:no-console
-    console.error(`${chalk[errorColour](error.type)} in ${chalk.underline(error.packageName)}: ${error.message}`);
+  if (errors) {
+    for (const error of errors) {
+      const errorColour = error.type === 'Error' ? 'red' : 'yellow';
+      // tslint:disable-next-line:no-console
+      console.error(`${chalk[errorColour](error.type)} in ${chalk.underline(error.packageName)}: ${error.message}`);
+    }
+    process.exit(1);
   }
-  process.exit(1);
 });

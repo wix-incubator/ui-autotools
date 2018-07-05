8c0a6759813a9776290828f464f1dab4a938a072
Kieran Williams <kieranw@wix.com>
2018-07-05 16:08:31 +0300

    Stash

--- packages/sanity/src/ssr-renderer/browser/run.tsx
+++ packages/sanity/src/ssr-renderer/browser/run.tsx
@@ -1,8 +1,8 @@
-import Registry from 'metadata-tools';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {test} from '../test/sample.spec';
-import 'mocha';
+// import Registry from 'metadata-tools';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import {ssrTest} from '../../ssr-test/ssr-test';
+import Mocha from 'mocha';
 
 // [ISSUE] If the registry imported here and the registry imported by the
 // project sit in different node_modules we're screwed.
@@ -13,31 +13,46 @@ import 'mocha';
 // uniquness of those titles because tests in Eyes are identified by their
 // title.
 
-const reactRoot = document.querySelector('#react-root')!;
+// const reactRoot = document.querySelector('#react-root')!;
 
-function createTestsFromSimulations() {
-  const tests = [];
-  for (const [Comp, meta] of Registry.metadata.components.entries()) {
-    for (const [simIndex, sim] of meta.simulations.entries()) {
-      tests.push({
-        title: Comp.name + ' ' + simIndex,
-        render:  () => ReactDOM.render(<Comp {...sim.props} />, reactRoot),
-        cleanup: () => ReactDOM.unmountComponentAtNode(reactRoot)
-      });
-    }
-  }
-  return tests;
-}
+// function createTestsFromSimulations() {
+//   const tests = [];
+//   for (const [Comp, meta] of Registry.metadata.components.entries()) {
+//     for (const [simIndex, sim] of meta.simulations.entries()) {
+//       tests.push({
+//         title: Comp.name + ' ' + simIndex,
+//         render:  () => ReactDOM.render(<Comp {...sim.props} />, reactRoot),
+//         cleanup: () => ReactDOM.unmountComponentAtNode(reactRoot)
+//       });
+//     }
+//   }
+//   return tests;
+// }
+
+const mocha = new Mocha();
+
+// Grab the ssr-test.js file
+const pathToTest = require.resolve('../../ssr-test/ssr-test');
+mocha.addFile(pathToTest);
+mocha.reporter('bdd');
+
+// Invoking this method runs our ssr-test in the mocha environment
+const autoSSRTest = () => {
+  // Run the ssr-test file
+  mocha.run((failures: number) => {
+    process.exitCode = failures ? -1 : 0;
+  });
+};
 
 function run() {
   // Puppeteer decides which tests to run and in what order, we just provide it
   // with the list of test titles and expose hooks for render and cleanup.
   // createTestsFromSimulations();
-  const comps = createTestsFromSimulations();
-  test();
-  comps.map((comp) => {
-    comp.render();
-  });
+  // const comps = createTestsFromSimulations();
+  autoSSRTest();
+  // comps.map((comp) => {
+  //   comp.render();
+  // });
   // (window as any).puppeteerRenderTest  = (i: number) => tests[i].render();
   // (window as any).puppeteerCleanupTest = (i: number) => tests[i].cleanup();
   // (window as any).puppeteerRunTests(tests.map(({title}) => ({title})));

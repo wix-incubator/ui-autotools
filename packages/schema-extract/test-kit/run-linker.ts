45b310a5fbe30753e2f42437db32d4093f270df0
Nadav Abrahami <nadav@lan.wixpress.com>
2018-07-10 16:40:15 +0300

    start work on schema linker tests

--- packages/schema-extract/test-kit/run-linker.ts
+++ packages/schema-extract/test-kit/run-linker.ts
@@ -1,32 +1,24 @@
 import * as ts from 'typescript';
 import { transform } from '../src/file-transformer';
-import { MemoryFileSystem } from 'kissfs';
+import { MemoryFileSystem, DirectoryContent } from 'kissfs';
 import { createHost } from '../src/isomorphc-typescript-host';
-import { ModuleSchema } from '../src/json-schema-types';
+import {  Schema } from '../src/json-schema-types';
 
-export function transformTest(source: string, moduleId: string): ModuleSchema<any> {
+
+
+
+export async function linkTest(sourceDir: DirectoryContent, modulePath: string, entityName:string): Promise<Schema> {
     const memFs = new MemoryFileSystem();
     const projectName = 'someProject';
-    const testedPath = '/' + projectName + '/src/tested-module';
+    const testedPath = '/' + projectName + modulePath;
     const testedFile = testedPath + '.ts';
-    const fixture = `
-    export type AType  = string;
-    export class AClass{
 
-    }
-    export class AGenericClass<T,Q>{
-
-    }`;
-    MemoryFileSystem.addContent(memFs, {
-        [projectName]: {
-            src: {
-                'tested-module.ts': source,
-                'test-assets.ts': fixture,
-            },
-        },
-
-    });
+    MemoryFileSystem.addContent(memFs, sourceDir);
     const prg = ts.createProgram([testedFile, projectName + '/src/test-assets.ts'], {}, createHost(memFs));
     const chckr = prg.getTypeChecker();
-    return transform(chckr, prg.getSourceFile(testedFile)!, '/src/' + moduleId, '/' + projectName);
+
+    const linker = new SchemaLinker(prg, chckr)
+
+
+    return await linker.flatten(testedFile,entityName);
 }

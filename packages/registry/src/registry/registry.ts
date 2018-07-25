31830cb5c9cf9a7204e028a42066668e6e02b505
Kieran Williams <kieranw@wix.com>
2018-07-25 15:31:50 +0300

    Only declaring newCompName once

--- packages/registry/src/registry/registry.ts
+++ packages/registry/src/registry/registry.ts
@@ -7,13 +7,13 @@ import {getCompName} from '../utils/get-comp-name';
 const Registry: IRegistry = {
   metadata: new Metadata(),
   getComponentMetadata <Props>(comp: ComponentType<Props>): ComponentMetadata<Props> {
-    if (!getCompName(comp)) {
-      throw new Error('Components must have either a "name" property, or a "displayName" property.');
+    const newCompName = getCompName(comp);
+
+    if (!newCompName) {
+      throw new Error('Components must have a "name" property, or a "displayName" property.');
     }
 
     if (!this.metadata.components.has(comp)) {
-      const newCompName = getCompName(comp);
-
       for (const component of this.metadata.components.keys()) {
         if (getCompName(component) === newCompName) {
           throw new Error(`There's already a component with the name: "${newCompName}". Component names should be unique.`);

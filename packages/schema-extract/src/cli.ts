65dfbc93f3a60d6498c3e9b98c15d1b65aa78973
Daniel Strokovsky <danielstr@wix.com>
2018-07-30 14:37:53 +0300

    changed linked cli command to return a more reasonable response. It is still not fully working yet...

--- packages/schema-extract/src/cli.ts
+++ packages/schema-extract/src/cli.ts
@@ -5,7 +5,7 @@ import {inspect} from 'util';
 import commander from 'commander';
 import chalk from 'chalk';
 import {LocalFileSystem} from 'kissfs';
-import {extractSchema, extractLinkedSchema} from './extract-schema';
+import {extractSchema} from './extract-schema';
 
 commander
   .version('0.1.0')
@@ -21,12 +21,12 @@ async function run() {
   const linked = commander.linked as boolean;
 
   const fs = new LocalFileSystem(inputDir);
-  const schemas = linked ? extractLinkedSchema(inputDir, sourcesGlob) : extractSchema(inputDir, sourcesGlob);
-  for (const {file, schema} of schemas) {
+  const schemas = extractSchema(inputDir, sourcesGlob, linked);
+  for (const {file, schema, linkedSchema} of schemas) {
     const relativePath = posix.relative(inputDir, file);
     if (outputDir) {
       const outputFilename = posix.join(outputDir, relativePath + '.json');
-      await fs.saveFile(outputFilename, JSON.stringify(schema, null, 4));
+      await fs.saveFile(outputFilename, linked ? linkedSchema! : JSON.stringify(schema, null, 4));
     } else {
       process.stdout.write('\n');
       process.stdout.write('\n' + chalk.yellow(relativePath));
--- packages/schema-extract/src/extract-schema.ts
+++ packages/schema-extract/src/extract-schema.ts
@@ -6,22 +6,7 @@ import {transform} from './file-transformer';
 import {createHost} from './isomorphc-typescript-host';
 import {SchemaLinker} from './file-linker';
 
-export function* extractSchema(basePath: string, filesGlob: string) {
-  const files = glob.sync(filesGlob, {cwd: basePath});
-  const host = createHost(new LocalFileSystem(basePath));
-  const program = typescript.createProgram(files, {}, host);
-  const checker = program.getTypeChecker();
-  for (const file of files) {
-    const source = program.getSourceFile(file);
-    const schema = transform(checker, source!, file, '');
-    yield {
-      file: path.join(basePath, file),
-      schema
-    };
-  }
-}
-
-export function* extractLinkedSchema(basePath: string, filesGlob: string) {
+export function* extractSchema(basePath: string, filesGlob: string, linked = false) {
   const files = glob.sync(filesGlob, {cwd: basePath});
   const host = createHost(new LocalFileSystem(basePath));
   const program = typescript.createProgram(files, {}, host);
@@ -30,12 +15,12 @@ export function* extractLinkedSchema(basePath: string, filesGlob: string) {
     let linkedSchema;
     const source = program.getSourceFile(file);
     const schema = transform(checker, source!, file, '');
-    if (schema.definitions) {
+    if (linked && schema.definitions) {
       const linkedSchemas = [];
       for (const definition in schema.definitions ) {
         if (schema.definitions.hasOwnProperty(definition)) {
           const linker = new SchemaLinker(program, checker, path.join(basePath, file));
-          linkedSchemas.push(linker.flatten(file, definition));
+          linkedSchemas.push(JSON.stringify(linker.flatten(file, definition), null, 4));
         }
       }
       linkedSchema = linkedSchemas.join('\n\n');

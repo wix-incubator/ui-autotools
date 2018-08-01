f1117fbd5033a14faa777e0e4de745a0b8880fd9
Kieran Williams <kieranw@wix.com>
2018-08-01 14:14:43 +0300

    Changed react imports from 'import React' to 'import * as React'

--- packages/a11y/src/browser/run.tsx
+++ packages/a11y/src/browser/run.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import Registry, {getCompName} from '@ui-autotools/registry';
 import axe from 'axe-core';
 
--- packages/sanity/src/ssr-test/ssr-test.tsx
+++ packages/sanity/src/ssr-test/ssr-test.tsx
@@ -1,7 +1,7 @@
 /**
  * @jest-environment node
  */
-import React from 'react';
+import * as React from 'react';
 import {renderToString} from 'react-dom/server';
 import Registry, {getCompName} from '@ui-autotools/registry';
 import {expect} from 'chai';

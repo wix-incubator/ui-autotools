5a974ebf4d8a7659595d5620c8b1d2ef0e3da830
Kieran Williams <kieranw@wix.com>
2018-08-01 14:47:13 +0300

    Changed react imports from 'import React' to 'import * as React' (#75)

--- packages/mock-repo/src/composite/composite.meta.tsx
+++ packages/mock-repo/src/composite/composite.meta.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import Registry from '@ui-autotools/registry';
 import {Composite} from './composite';
 
--- packages/registry/src/registry/component-metadata.tsx
+++ packages/registry/src/registry/component-metadata.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import {IComponentMetadata, ISimulation, IStyleMetadata} from './types';
 import {isValidSimulationTitle} from '../utils';
 

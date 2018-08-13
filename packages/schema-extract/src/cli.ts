bdb03e67277e4abfdb44f7a51ea82b001a8bf41a
Daniel Strokovsky <danielstr@wix.com>
2018-08-13 13:52:06 +0300

    cleaning cli

--- packages/schema-extract/src/cli.ts
+++ packages/schema-extract/src/cli.ts
@@ -5,7 +5,7 @@ import {inspect} from 'util';
 import commander from 'commander';
 import chalk from 'chalk';
 import {LocalFileSystem} from 'kissfs';
-import {extractSchema, extractLinkedSchema} from './extract-schema';
+import {extractLinkedSchema} from './extract-schema';
 
 commander
   .version('0.1.0')
@@ -19,31 +19,17 @@ async function run() {
   const outputDir = commander.output as string;
   const sourcesGlob = commander.files as string;
   const linked = commander.linked as boolean;
-
   const fs = new LocalFileSystem(inputDir);
-  if (linked) {
-    for (const {file, linkedSchema} of extractLinkedSchema(inputDir, sourcesGlob)) {
-      const relativePath = posix.relative(inputDir, file);
-      if (outputDir) {
-        const outputFilename = posix.join(outputDir, relativePath + '.json');
-        await fs.saveFile(outputFilename, JSON.stringify(linkedSchema, null, 4));
-      } else {
-        process.stdout.write('\n');
-        process.stdout.write('\n' + chalk.yellow(relativePath));
-        process.stdout.write('\n' + inspect(linkedSchema, {depth: null, colors: true}));
-      }
-    }
-  } else {
-    for (const {file, schema} of extractSchema(inputDir, sourcesGlob)) {
-      const relativePath = posix.relative(inputDir, file);
-      if (outputDir) {
-        const outputFilename = posix.join(outputDir, relativePath + '.json');
-        await fs.saveFile(outputFilename, JSON.stringify(schema, null, 4));
-      } else {
-        process.stdout.write('\n');
-        process.stdout.write('\n' + chalk.yellow(relativePath));
-        process.stdout.write('\n' + inspect(schema, {depth: null, colors: true}));
-      }
+
+  for (const {file, schema, linkedSchema} of extractLinkedSchema(inputDir, sourcesGlob)) {
+    const relativePath = posix.relative(inputDir, file);
+    if (outputDir) {
+      const outputFilename = posix.join(outputDir, relativePath + '.json');
+      await fs.saveFile(outputFilename, JSON.stringify(linked ? linkedSchema : schema, null, 4));
+    } else {
+      process.stdout.write('\n');
+      process.stdout.write('\n' + chalk.yellow(relativePath));
+      process.stdout.write('\n' + inspect(linkedSchema, {depth: null, colors: true}));
     }
   }
 }

fb369e27aa2c5597a679fd5df0581b14382981d6
Daniel Strokovsky <danielstr@wix.com>
2018-08-28 13:55:34 +0300

    initial commit

--- packages/schema-extract/src/extract-schema.ts
+++ packages/schema-extract/src/extract-schema.ts
@@ -6,13 +6,15 @@ import {transform} from './file-transformer';
 import {createHost} from './isomorphc-typescript-host';
 import {SchemaLinker} from './file-linker';
 
+let program: typescript.Program;
+
 export function* extractSchema(basePath: string, filesGlob: string) {
   const files = glob.sync(filesGlob, {cwd: basePath});
   const host = createHost(new LocalFileSystem(basePath));
-  const program = typescript.createProgram(files, {}, host);
-  const checker = program.getTypeChecker();
+  const prg = typescript.createProgram(files, {}, host);
+  const checker = prg.getTypeChecker();
   for (const file of files) {
-    const source = program.getSourceFile(file);
+    const source = prg.getSourceFile(file);
     const schema = transform(checker, source!, file, '');
     yield {
       file: path.join(basePath, file),
@@ -21,8 +23,14 @@ export function* extractSchema(basePath: string, filesGlob: string) {
   }
 }
 
+export function createLinkerProgram(files: string[]) {
+  program = typescript.createProgram(files, {});
+}
+
 export function getSchema(filePath: string, exportName: string) {
-  const program = typescript.createProgram([filePath], {});
+  if (!program) {
+    program = typescript.createProgram([filePath], {});
+  }
   const checker = program.getTypeChecker();
   const linker = new SchemaLinker(program, checker, filePath);
   return linker.flatten(filePath, exportName);
@@ -30,12 +38,12 @@ export function getSchema(filePath: string, exportName: string) {
 
 export function* extractLinkedSchema(basePath: string, filesGlob: string) {
   const files = glob.sync(filesGlob, {cwd: basePath});
-  const program = typescript.createProgram(files, {});
-  const checker = program.getTypeChecker();
+  const prg = typescript.createProgram(files, {});
+  const checker = prg.getTypeChecker();
   for (const file of files) {
     const linkedSchema: any = {};
-    const linker = new SchemaLinker(program, checker, path.join(basePath, file));
-    const source = program.getSourceFile(file);
+    const linker = new SchemaLinker(prg, checker, path.join(basePath, file));
+    const source = prg.getSourceFile(file);
     const schema = transform(checker, source!, file, '');
     if (schema.definitions) {
       linkedSchema.properties = schema.properties;

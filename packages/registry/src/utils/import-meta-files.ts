f2b473f81fd200ee5d0d23532767defeeb188fd1
Kieran Williams <kieranw@wix.com>
2018-09-05 11:01:07 +0300

    Fix issue with basepath parameter being undefined (#125)

--- packages/registry/src/utils/import-meta-files.ts
+++ packages/registry/src/utils/import-meta-files.ts
@@ -8,9 +8,13 @@ const importMeta = (filePattern?: string, basepath?: string) => {
   const options: glob.IOptions = {
     nosort: true,
     matchBase: true,
-    absolute: true,
-    cwd: basepath
+    absolute: true
   };
+
+  if (basepath) {
+    options.cwd = basepath;
+  }
+
   const defaultPattern = './**/*.meta.ts[x?]';
 
   const files = glob.sync(filePattern || defaultPattern, options);

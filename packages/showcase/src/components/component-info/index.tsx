b3592f1addd80a2a42acbea054432fb70f0d7089
Alexey Lebedev <alexeyl@wix.com>
2018-09-05 11:06:11 +0300

    Showcase: style dropdown

--- packages/showcase/src/components/component-info/index.tsx
+++ packages/showcase/src/components/component-info/index.tsx
@@ -9,7 +9,7 @@ interface IComponentInfoProps {
 
 export class ComponentInfo extends React.Component<IComponentInfoProps> {
   public render() {
-    const {name, schema, simulationTitles} = this.props.component;
+    const {name, schema, simulationTitles, styleTitles} = this.props.component;
 
     return (
       <div>
@@ -27,7 +27,11 @@ export class ComponentInfo extends React.Component<IComponentInfoProps> {
         </pre>
 
         <h2>Simulations</h2>
-        <Simulations componentName={name} simulationTitles={simulationTitles} />
+        <Simulations
+          componentName={name}
+          simulationTitles={simulationTitles}
+          styleTitles={styleTitles}
+        />
       </div>
     );
   }
@@ -36,50 +40,88 @@ export class ComponentInfo extends React.Component<IComponentInfoProps> {
 interface ISimulationsProps {
   componentName: string;
   simulationTitles: string[];
+  styleTitles: string[];
 }
 
 interface ISimulationsState {
   selectedSimulation?: string;
+  selectedStyle?: string;
 }
 
 class Simulations extends React.Component<ISimulationsProps, ISimulationsState> {
   public state = {
-    selectedSimulation: this.props.simulationTitles[0]
+    selectedSimulation: this.props.simulationTitles[0],
+    selectedStyle: this.props.styleTitles[0]
   };
 
   public render() {
-    const {componentName, simulationTitles} = this.props;
+    const {componentName, simulationTitles, styleTitles} = this.props;
     if (!simulationTitles.length) {
       return null;
     }
 
-    const {selectedSimulation} = this.state;
+    const {selectedSimulation, selectedStyle} = this.state;
     const src = (
       `/simulation.html?` +
-      `component=${componentName}&` +
-      `simulation=${selectedSimulation}`
+      `component=${encodeURIComponent(componentName)}&` +
+      `simulation=${encodeURIComponent(selectedSimulation || '')}&` +
+      `style=${encodeURIComponent(selectedStyle || '')}`
     );
 
     return (
       <div>
         <div>
-          <select onChange={this.handleChange}>
-            {simulationTitles.map((title) =>
-              <option key={title} selected={title === selectedSimulation}>
-                {title}
-              </option>
-            )}
-          </select>
+          {this.renderSimulationsDropdown(simulationTitles, selectedSimulation)}
+          {' \u00A0 '}
+          {styleTitles.length ?
+            this.renderStylesDropdown(styleTitles, selectedStyle) : null
+          }
         </div>
         <br />
         <div>
-          <iframe width={400} height={200} src={src} />
+          <iframe width="100%" height={400} src={src} />
         </div>
       </div>
     );
   }
 
-  private handleChange: React.ChangeEventHandler<HTMLSelectElement> = (event) => {
+  private renderSimulationsDropdown(titles: string[], selectedTitle: string) {
+    return (
+      <label>
+        Simulation{' '}
+        <select
+          value={selectedTitle}
+          onChange={this.handleSimulationChange}
+        >
+          {titles.map((title) =>
+            <option key={title} value={title}>{title}</option>
+          )}
+        </select>
+      </label>
+    );
+  }
+
+  private renderStylesDropdown(titles: string[], selectedTitle: string) {
+    return (
+      <label>
+        Style{' '}
+        <select
+          value={selectedTitle}
+          onChange={this.handleStyleChange}
+        >
+          {titles.map((title) =>
+            <option key={title} value={title}>{title}</option>
+          )}
+        </select>
+      </label>
+    );
+  }
+
+  private handleSimulationChange: React.ChangeEventHandler<HTMLSelectElement> = (event) => {
     this.setState({selectedSimulation: event.target.value});
+  }
+
+  private handleStyleChange: React.ChangeEventHandler<HTMLSelectElement> = (event) => {
+    this.setState({selectedStyle: event.target.value});
   }
 }

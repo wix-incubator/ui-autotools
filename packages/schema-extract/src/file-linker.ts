f16294033a83174497f9380e6b182a0d2279b583
Daniel Strokovsky <danielstr@wix.com>
2018-10-03 13:46:03 +0300

    code cleaning

--- packages/schema-extract/src/file-linker.ts
+++ packages/schema-extract/src/file-linker.ts
@@ -207,9 +207,9 @@ export class SchemaLinker {
                     if (option.enum) {
                         res.enum = option.enum;
                     }
-                    if (option.definedAt) {
-                        res.definedAt = option.definedAt;
-                    }
+                    // if (option.definedAt) {
+                    //     res.definedAt = option.definedAt;
+                    // }
                 } else {
                     if (Object.keys(res).length === 0) {
                         res = option;
@@ -217,9 +217,9 @@ export class SchemaLinker {
                         if (option.enum) {
                             if (!res.enum) {
                                 res.enum = option.enum;
-                                if (option.definedAt) {
-                                    res.definedAt = option.definedAt;
-                                }
+                                // if (option.definedAt) {
+                                //     res.definedAt = option.definedAt;
+                                // }
                             } else {
                                 const enums = [];
                                 for (let i = 0; i < option.enum.length; i++) {
@@ -234,12 +234,15 @@ export class SchemaLinker {
                                 }
                             }
                         }
-                        if (option.definedAt) {
-                            res.definedAt = option.definedAt;
-                        }
+                        // if (option.definedAt) {
+                        //     res.definedAt = option.definedAt;
+                        // }
                     } else {
                         return {$ref: NeverId};
                     }
+                }
+                if (option.definedAt) {
+                    res.definedAt = option.definedAt;
                 }
             }
         }

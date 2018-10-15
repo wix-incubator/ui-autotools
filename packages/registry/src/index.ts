1e8eee6e2424b033bca794006ebc2162e5d95279
Kieran Williams <kieranw@wix.com>
2018-10-15 11:27:38 +0300

    Added mock-registry

--- packages/registry/src/index.ts
+++ packages/registry/src/index.ts
@@ -1,4 +1,5 @@
 import Registry from './registry/registry';
+export * from '../test-kit/mock-registry';
 export * from './registry';
 export * from './utils';
 export default Registry;
---
+++ packages/registry/test-kit/mock-registry.ts
@@ -0,0 +1,28 @@
+import Registry from '../src/registry/registry';
+import {MockComp} from './fixtures/mock-comp';
+import style from './fixtures/variant.st.css';
+
+const mockMetadata = Registry.getComponentMetadata(MockComp);
+
+mockMetadata.exportedFrom({
+  baseStylePath: 'test-kit/fixtures/base-style.st.css',
+  exportName: 'MockComp',
+  path: 'test-kit/fixtures/mock-comp'
+});
+
+mockMetadata.addSim({
+  title: 'mock simulation',
+  props: {
+    propsLabel: 'props sim'
+  },
+  state: {
+    stateLabel: 'state sim'
+  }
+});
+
+mockMetadata.addStyle(style, {
+  name: 'variant',
+  path: 'test-kit/fixtures/variant.st.css'
+});
+
+export {Registry as MockRegistry};

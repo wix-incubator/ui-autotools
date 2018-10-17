06c987f6de57b1c8bed71553adb42358904d9661
Kieran Williams <kieranw@wix.com>
2018-10-17 11:02:20 +0300

    Formatting

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,7 +1,7 @@
 import {buildBaseFiles} from './generate-snapshots/build-base-files';
 import {generateSnapshots} from './generate-snapshots/generate-snapshots';
 import {runEyes} from './snap-test/snap';
-import { createTempDirectory } from 'create-temp-directory';
+import {createTempDirectory} from 'create-temp-directory';
 import Registry from '@ui-autotools/registry';
 
 export async function eyesTest(projectPath: string, fs: any) {

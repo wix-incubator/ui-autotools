8e1967c5ee34d229f62ec87695b491734d5ec784
Kieran Williams <kieranw@wix.com>
2018-10-18 17:23:22 +0300

    Testing output data

---
+++ packages/snap/test/fixtures/MockComp@0@mock simulation@variant.snapshot.ts
@@ -0,0 +1,3 @@
+import style from '../../src/registry/fixtures/variant.st.css';
+import {MockComp} from '../../src/registry/fixtures/mock-comp';
+export default {comp: MockComp, name: 'MockComp', style};
--- packages/snap/test/generate-index-files.spec.ts
+++ packages/snap/test/generate-index-files.spec.ts
@@ -2,10 +2,11 @@ import { expect } from 'chai';
 import {MockRegistry, mockData} from '@ui-autotools/test-kit';
 import {generateIndexFileData} from '../src/generate-snapshots/build-base-files';
 
-describe('Generating index files', () => {
-  it('generates index files from a component and its variants', () => {
+describe('Generating index file data', () => {
+  it('generates the correct data', () => {
     const data = generateIndexFileData(MockRegistry, '');
-    console.log('data', data);
-    expect(data.f).to.equal(true);
+    expect(data[0].basename).to.equal('MockComp@0@mock simulation@variant');
+    expect(data[0].filepath).to.equal('MockComp@0@mock simulation@variant.snapshot.ts');
+    expect(data[0].data).to.equal('import style from \'../../src/registry/fixtures/variant.st.css\';\nimport {MockComp} from \'../../src/registry/fixtures/mock-comp\';\nexport default {comp: MockComp, name: \'MockComp\', style};\n');
   });
 });

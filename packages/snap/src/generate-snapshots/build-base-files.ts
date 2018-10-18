f263ef5ac838b4b814b52055cbe27f1f254130b9
Kieran Williams <kieranw@wix.com>
2018-10-18 14:34:04 +0300

    MOved method to proper place

--- packages/snap/src/generate-snapshots/build-base-files.ts
+++ packages/snap/src/generate-snapshots/build-base-files.ts
@@ -1,8 +1,9 @@
-import { IRegistry } from '@ui-autotools/registry';
-import { consoleLog } from '@ui-autotools/utils';
+import path from 'path';
+import {generateSnapshotFilename, generateData} from './filename-utils';
+import {IRegistry} from '@ui-autotools/registry';
+import {consoleLog} from '@ui-autotools/utils';
 import {createAutotoolsFolder} from './create-autotools-folder';
-import { writeDataToFs } from './write-data-to-fs';
-import { generateIndexFileData } from './generate-index-file-data';
+import {writeDataToFs} from './write-data-to-fs';
 
 export interface IFileInfo {
   basename: string;
@@ -13,10 +14,44 @@ export interface IFileInfo {
 const stylePathPrefix = '../../'; // We're two folders deep in .autotools
 const compPathPrefix = '../../';
 
+export function generateIndexFileData(Registry: IRegistry, autotoolsFolder: string): IFileInfo[]  {
+  const files: IFileInfo[] = [];
+
+  Registry.metadata.components.forEach((componentMetadata) => {
+    const simIndex = componentMetadata.simulations.length;
+    const styles = componentMetadata.styles;
+    const compPath =  path.join(compPathPrefix, componentMetadata.path);
+    const compName = componentMetadata.exportName;
+
+    if (compName) {
+      for (let i = 0; i < simIndex; i++) {
+        const simulationName = componentMetadata.simulations[i].title;
+        if (styles.size) {
+          styles.forEach((style) => {
+            const stylePath = path.join(stylePathPrefix, style.path);
+            const basename = generateSnapshotFilename(compName, simulationName, i, style.name);
+            const filepath = path.join(autotoolsFolder, basename + '.snapshot.ts');
+            const data = generateData(compName, compPath, stylePath);
+            files.push({basename, filepath, data});
+          });
+        } else {
+          // We only want to render the base style if there are no other style variants
+          const basename = generateSnapshotFilename(compName, simulationName, i);
+          const filepath = path.join(autotoolsFolder, basename, '.snapshot.ts');
+          const data = generateData(compName, compPath);
+          files.push({basename, filepath, data});
+        }
+      }
+    }
+  });
+
+  return files;
+}
+
 export const buildBaseFiles = (projectPath: string, Registry: IRegistry): IFileInfo[] => {
   consoleLog('Building base files...');
   const autotoolsFolder = createAutotoolsFolder(projectPath);
-  const files = generateIndexFileData(Registry, autotoolsFolder, compPathPrefix, stylePathPrefix);
+  const files = generateIndexFileData(Registry, autotoolsFolder);
   writeDataToFs(files);
 
   return files;

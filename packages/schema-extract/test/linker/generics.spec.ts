079c48e0f3fdc2c6193c1d5b9358fd208e5e0ac2
Daniel Strokovsky <danielstr@wix.com>
2018-10-18 09:01:02 +0300

    added renamed file

---
+++ packages/schema-extract/test/linker/generics.spec.ts
@@ -0,0 +1,88 @@
+import {expect} from 'chai';
+import { Schema, ClassSchemaId } from '../../src/json-schema-types';
+import {linkTest} from '../../test-kit/run-linker';
+
+describe('schema-linker - generic types', () => {
+    it('should flatten genric type definition', async () => {
+        const fileName = 'index.ts';
+        const res = linkTest({[fileName]: `
+        export type MyType<T, W> = {
+            something:W;
+            someone: T;
+        };
+        export type B = MyType<string, number>;
+        `}, 'B', fileName);
+
+        const expected: Schema<'object'> = {
+            type: 'object',
+            properties: {
+                something: {
+                    type: 'number'
+                },
+                someone: {
+                    type: 'string'
+                }
+            },
+            required: ['something', 'someone']
+        };
+        expect(res).to.eql(expected);
+    });
+
+    it('should deep flatten genric type definition', async () => {
+        const fileName = 'index.ts';
+        const res = linkTest({[fileName]: `
+        export type MyType<T> = {
+            something: {
+                a: T;
+            };
+        };
+        export type B = MyType<string>;
+        `}, 'B', fileName);
+
+        const expected: Schema<'object'> = {
+            type: 'object',
+            properties: {
+                something: {
+                    type: 'object',
+                    properties: {
+                        a: {
+                            type: 'string'
+                        }
+                    },
+                    required: ['a']
+                }
+            },
+            required: ['something']
+        };
+        expect(res).to.eql(expected);
+    });
+
+    it('should deep flatten genric class definition', async () => {
+        const fileName = 'index.ts';
+        const res = linkTest({[fileName]: `
+        export class A<T>{
+            b: {b: T}
+        };
+
+        export class B extends A<string>{};
+        `}, 'B', fileName);
+
+        const expected = {
+            $ref: ClassSchemaId,
+            extends: {
+                $ref: '#A',
+            },
+            staticProperties: {},
+            properties: {
+                b: {
+                    inheritedFrom: '#A',
+                    properties: {
+                        b: {type: 'string'}
+                    },
+                    required: ['b']
+                }
+            }
+        };
+        expect(res).to.eql(expected);
+    });
+});

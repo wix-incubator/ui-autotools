33412f1b7d6b6a59b34e4204fe6d2bb75047fc2d
Kieran Williams <kieranw@wix.com>
2018-10-22 11:17:55 +0300

    Remove fixtures file

--- packages/snap/test/fixtures/MockComp@0@mock simulation@variant.snapshot.ts
+++
@@ -1,3 +0,0 @@
-import style from '../../src/registry/fixtures/variant.st.css';
-import {MockComp} from '../../src/registry/fixtures/mock-comp';
-export default {comp: MockComp, name: 'MockComp', style};

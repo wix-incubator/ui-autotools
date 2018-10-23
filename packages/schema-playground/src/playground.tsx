8667b26a1536d4c32e3cd39fcbea05b7022c05cc
Avi Vahl <avi.vahl@wix.com>
2018-10-23 14:06:27 +0300

    Live show schema of code

--- packages/schema-playground/src/playground.tsx
+++ packages/schema-playground/src/playground.tsx
@@ -2,6 +2,7 @@ import ts from 'typescript';
 import React from 'react';
 import { IFileSystem } from '@file-services/types';
 import { IBaseHost } from '@file-services/typescript';
+import { transform } from '@ui-autotools/schema-extract/esm/file-transformer';
 
 import 'sanitize.css';
 import './playground.css';
@@ -15,11 +16,13 @@ export interface IPlaygroundProps {
 
 export interface IPlaygroundState {
     transpiledOutput: string;
+    schema: string;
 }
 
 export class Playground extends React.PureComponent<IPlaygroundProps, IPlaygroundState> {
     public state = {
-        transpiledOutput: ''
+        transpiledOutput: '',
+        schema: ''
     };
 
     public componentDidMount() {
@@ -32,17 +35,20 @@ export class Playground extends React.PureComponent<IPlaygroundProps, IPlaygroun
         return (
             <>
                 <textarea
+                    spellCheck={false}
                     value={readFileSync(filePath)}
                     onChange={this.onInputChange}
                     style={{ width: 500, height: 500 }}
                 />
                 <textarea
+                    spellCheck={false}
                     value={this.state.transpiledOutput}
                     readOnly={true}
                     style={{ width: 500, height: 500 }}
                 />
                 <textarea
-                    value={this.getFormattedDiagnostics()}
+                    spellCheck={false}
+                    value={this.state.schema}
                     readOnly={true}
                     style={{ width: 500, height: 500 }}
                 />
@@ -51,7 +57,17 @@ export class Playground extends React.PureComponent<IPlaygroundProps, IPlaygroun
     }
 
     private transpileFile() {
-        this.setState({ transpiledOutput: this.getTranspiledCode() });
+        const transpiledOutput = this.getTranspiledCode();
+        const program = this.props.languageService.getProgram();
+        const typeChecker = program && program.getTypeChecker();
+        const sourceFile = program && program.getSourceFile(this.props.filePath);
+        if (typeChecker && sourceFile) {
+            const schema = JSON.stringify(transform(typeChecker, sourceFile, this.props.filePath, '/'), null, 2);
+            this.setState({ transpiledOutput, schema });
+        } else {
+            this.setState({ transpiledOutput });
+        }
+
     }
 
     private onInputChange: React.ChangeEventHandler<HTMLTextAreaElement> = (e) => {
@@ -71,12 +87,12 @@ export class Playground extends React.PureComponent<IPlaygroundProps, IPlaygroun
         return jsFile.text;
     }
 
-    private getFormattedDiagnostics(): string {
-        const { baseHost, languageService, filePath } = this.props;
-        const diagnostics = [
-            ...languageService.getSyntacticDiagnostics(filePath),
-            ...languageService.getSemanticDiagnostics(filePath)
-        ];
-        return ts.formatDiagnostics(diagnostics, baseHost);
-    }
+    // private getFormattedDiagnostics(): string {
+    //     const { baseHost, languageService, filePath } = this.props;
+    //     const diagnostics = [
+    //         ...languageService.getSyntacticDiagnostics(filePath),
+    //         ...languageService.getSemanticDiagnostics(filePath)
+    //     ];
+    //     return ts.formatDiagnostics(diagnostics, baseHost);
+    // }
 }

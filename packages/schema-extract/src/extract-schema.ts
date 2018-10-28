0c6893d7a7b48d444a35369a7928fb1bf7b544ce
Daniel Strokovsky <danielstr@wix.com>
2018-10-28 11:28:21 +0200

    added node types to tsconfig and removed extractSchema function

--- packages/schema-extract/src/extract-schema.ts
+++ packages/schema-extract/src/extract-schema.ts
@@ -1,26 +1,7 @@
-import path from 'path';
-import glob from 'glob';
 import typescript from 'typescript';
-import {LocalFileSystem} from 'kissfs';
 import {transform, getSchemaFromImport} from './file-transformer';
-import {createHost} from './isomorphc-typescript-host';
 import {SchemaLinker, IExtractor} from './file-linker';
 import { ModuleSchema } from './json-schema-types';
-
-export function* extractSchema(basePath: string, filesGlob: string) {
-  const files = glob.sync(filesGlob, {cwd: basePath});
-  const host = createHost(new LocalFileSystem(basePath));
-  const program = typescript.createProgram(files, {}, host);
-  const checker = program.getTypeChecker();
-  for (const file of files) {
-    const source = program.getSourceFile(file);
-    const schema = transform(checker, source!, file, '');
-    yield {
-      file: path.join(basePath, file),
-      schema
-    };
-  }
-}
 
 export function createLinker(files: string[]) {
   const program = typescript.createProgram(files, {});
@@ -39,9 +20,9 @@ export function createLinker(files: string[]) {
   function getImport(importPath: string, ref: string, file: string): ModuleSchema | null {
     return getSchemaFromImport(importPath, ref, program, program.getSourceFile(file));
   }
-  const x: IExtractor = {
+  const extractor: IExtractor = {
     getSchema,
     getSchemaFromImport: getImport
   };
-  return new SchemaLinker(x);
+  return new SchemaLinker(extractor);
 }

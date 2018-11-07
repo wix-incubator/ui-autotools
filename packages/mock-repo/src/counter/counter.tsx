06cf8f40db3d5c96c32816e115f904ee296bcb62
Kieran Williams <kieranw@wix.com>
2018-11-07 16:15:54 +0200

    Reset counter

--- packages/mock-repo/src/counter/counter.tsx
+++ packages/mock-repo/src/counter/counter.tsx
@@ -9,15 +9,14 @@ export class Counter extends React.Component<{}, IState> {
   public state = { count: 0 };
 
   public render() {
-    const myOnClick = this.onClick.bind(this);
     return (
-      <button onClick={myOnClick}>
+      <button onClick={this.onClick}>
         Click me: {this.state.count}
       </button>
     );
   }
 
-  private onClick() {
+  private onClick = () => {
     this.setState({count: this.state.count + 1});
   }
 }

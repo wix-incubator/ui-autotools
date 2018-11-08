16ac20083306fac539538214a1b9594d61f1a867
Kieran Williams <kieranw@wix.com>
2018-11-08 11:39:47 +0200

    Add comments

--- packages/sanity/src/hydration-test/event-listener-test.tsx
+++ packages/sanity/src/hydration-test/event-listener-test.tsx
@@ -19,7 +19,11 @@ function assertNoListeners(listeners: {[event: string]: any}, context: string) {
 
 /**
  * Event Listener Test
- *
+ * The following test checks for event listeners that were not removed after component unmounting.
+ * It does this by overriding the default addEventListener and removeEventListener methods on
+ * window, document, and body, and proxying them with the original method in addition to our own
+ * method which tracks every event that was added or removed. Once the component is unmounted,
+ * we are able to check if any events are left.
  */
 
 export const eventListenerTest = (): void => {
@@ -31,6 +35,17 @@ export const eventListenerTest = (): void => {
 
     Registry.metadata.components.forEach((componentMetadata, Comp) => {
       describe(getCompName(Comp), () => {
+        /**
+         * When a React component adds an event listener in its render function (a la <div onClick={this.myOnClick} />),
+         * React does not add an event listener to the rendered node. Instead, React calls document.addEventListener and
+         * adds a single click event which it then uses to decide which components to call with a synthetic event if
+         * relevant. When the component is unmounted, this listener on the document remains, as React may use it for
+         * other components. This makes it difficult to know whether a component called document.addEventListener manually
+         * and then forgot to remove the listener during unmount, or whether they just used the jsx event syntax properly.
+         * To discern between these two cases, we render a component before testing which has a jsx event for every single
+         * React-supported event. This way, React will never call document.addEventListener during rendering of the components
+         * we want to test, and we can be sure that any remaining events are leftover.
+         */
         before(() => {
           ReactDOM.render(<AllEvents />, root);
           ReactDOM.unmountComponentAtNode(root);
@@ -40,7 +55,6 @@ export const eventListenerTest = (): void => {
           it('component should unmount without leaving event listeners on the window, document, and body', () => {
             const {windowEe, documentEe, bodyEe} = overrideEventListeners();
 
-            // Set root's HTML to the SSR component
             root.innerHTML = componentStrings[index];
             hydrate(componentMetadata.simulationToJSX(simulation), root);
             ReactDOM.unmountComponentAtNode(root);

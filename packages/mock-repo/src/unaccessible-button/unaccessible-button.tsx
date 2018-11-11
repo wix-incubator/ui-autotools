2b157c0647dc057cfb41cbb5c6675b743b557182
rashellek <rashellek@wix.com>
2018-11-11 15:27:25 +0200

    addec change in the run a11y function

--- packages/mock-repo/src/unaccessible-button/unaccessible-button.tsx
+++ packages/mock-repo/src/unaccessible-button/unaccessible-button.tsx
@@ -1,21 +1,12 @@
-// import * as React from 'react';
+import * as React from 'react';
 
-// interface IUnaccessibleButtonState {
-//   clicked: boolean;
-// }
+export class UnaccessibleButton extends React.Component<{}> {
 
-// export class UnaccessibleButton extends React.Component<{}, IUnaccessibleButtonState> {
-//   public state = { clicked: false };
-
-//   public render() {
-//     return (
-//       <button id="saveChanges" aria-label="" onClick={this.onClick}>
-//         Save as
-//       </button>
-//     );
-//   }
-
-//   private onClick = () => {
-//     this.setState({clicked: !this.state.clicked});
-//   }
-// }
+  public render() {
+    return (
+      <button id="saveChanges" role="NOT AN ARIA ROLE">
+        Save as
+      </button>
+    );
+  }
+}

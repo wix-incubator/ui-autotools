a61516a97c8e3eeca7a0b7d55902c41212be970d
Kieran Williams <kieranw@wix.com>
2018-11-18 17:49:28 +0200

    Testing snap PR hook

--- packages/mock-repo/run-snap-on-ci.ts
+++ packages/mock-repo/run-snap-on-ci.ts
@@ -2,5 +2,7 @@ import {execSync} from 'child_process';
 import isCI from 'is-ci';
 
 if (isCI) {
+  // tslint:disable-next-line:no-console
+  console.log('Running snap on CI.');
   execSync('yarn snap');
 }

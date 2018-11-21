48ca8b5aa2356fb611b3691de345d84236865519
Kieran Williams <kieranw@wix.com>
2018-11-21 15:56:08 +0200

    change script

--- packages/mock-repo/run-snap-on-ci.ts
+++ packages/mock-repo/run-snap-on-ci.ts
@@ -1,8 +1,13 @@
 import {execSync} from 'child_process';
+import {spawn} from 'child_process';
 import isCI from 'is-ci';
 
 if (isCI) {
   // tslint:disable-next-line:no-console
   console.log('Running snap on CI.');
-  execSync('yarn snap');
+  const snap = spawn('yarn snap');
+  snap.stdout.on('data', (data: any) => { process.stdout.write(data.toString()); });
+  snap.stderr.on('data', (data: any) => { process.stdout.write(data.toString()); });
+  // tslint:disable-next-line:no-console
+  snap.on('close', (code: number) => { console.log('Finished running snap with code ' + code); });
 }

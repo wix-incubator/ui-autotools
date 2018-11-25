7f2dc222a222874a52dced25ff8cc9478b57b1e9
Alexey Lebedev <alexeyl@wix.com>
2018-11-25 15:47:39 +0200

    schema-playground: register .d.ts files from the memory fs with the editor

--- packages/schema-playground/src/constants.ts
+++ packages/schema-playground/src/constants.ts
@@ -2,15 +2,32 @@ import ts from 'typescript';
 
 export const sampleTypescriptFilePath = '/index.tsx';
 export const sampleTypescriptFile = `
-import {Component} from 'react';
+import React, {Component} from 'react';
 
-export interface IProps { }
+export interface ICheckboxProps {
+  checked: boolean;
+  checkedIcon: React.ReactNode;
+  uncheckedIcon: React.ReactNode;
+  onChange?: () => void;
+}
 
-export class Comp extends Component<IProps> {
+export class Checkbox extends Component<ICheckboxProps> {
+  static defaultProps: Partial<ICheckboxProps> = {
+    onChange: () => null
+  };
+
   render() {
-    return <div />;
+    return (
+      <div onClick={this.props.onChange}>
+        {this.props.checked ?
+          this.props.checkedIcon :
+          this.props.uncheckedIcon
+        }
+      </div>
+    );
   }
 }
+
 `.trimLeft();
 
 export const sampleStylableFilePath = '/index.st.css';

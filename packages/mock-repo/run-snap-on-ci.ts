8474e43b721ad403d7fe0bc086514d3bd3c3e150
Kieran Williams <kieranw@wix.com>
2018-11-26 12:27:02 +0200

    Refactor CI script

--- packages/mock-repo/run-snap-on-ci.ts
+++ packages/mock-repo/run-snap-on-ci.ts
@@ -1,12 +1,12 @@
 import {spawn} from 'child_process';
-import isCI from 'is-ci';
+// import isCI from 'is-ci';
 
-if (isCI) {
+// if (isCI) {
   // tslint:disable-next-line:no-console
-  console.log('Running snap on CI.');
-  const snap = spawn('yarn autotools snap');
-  snap.stdout.on('data', (data: any) => { process.stdout.write(data.toString()); });
-  snap.stderr.on('data', (data: any) => { process.stdout.write(data.toString()); });
+console.log('Running snap on CI.');
+const snap = spawn('yarn', ['autotools', 'snap'], {cwd: './packages/mock-repo'});
+snap.stdout.on('data', (data: any) => { process.stdout.write(data.toString()); });
+snap.stderr.on('data', (data: any) => { process.stdout.write(data.toString()); });
   // tslint:disable-next-line:no-console
-  snap.on('close', (code: number) => { console.log('Finished running snap with code ' + code); });
-}
+snap.on('close', (code: number) => { console.log('Finished running snap with code ' + code); });
+// }

e63cbd4e1cbc6565e96f68e647867b93cd235350
Daniel Strokovsky <danielstr@wix.com>
2018-12-03 14:14:54 +0200

    small change

--- packages/utils/src/require-hooks/require-hooks.ts
+++ packages/utils/src/require-hooks/require-hooks.ts
@@ -3,9 +3,9 @@ import path from 'path';
 
 export function registerRequireHooks() {
   const projectPath = process.cwd();
-  const path1 = path.join(projectPath, '.autotools/node-require-hooks');
-  if (fs.existsSync(path1 + '.js')) {
-    require(path1);
+  const requirePath = path.join(projectPath, '.autotools/node-require-hooks.js');
+  if (fs.existsSync(requirePath)) {
+    require(requirePath);
   } else {
     require('@ts-tools/node');
     require('@stylable/node/register');

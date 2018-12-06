3bfde0e762985463a3bf45a09371e6e2145db0b4
Kieran Williams <kieranw@wix.com>
2018-12-06 17:38:59 +0200

    Fix script

--- packages/mock-repo/snap-if-ci.ts
+++ packages/mock-repo/snap-if-ci.ts
@@ -2,10 +2,11 @@ import isCI from 'is-ci';
 import {exec} from 'child_process';
 
 if (isCI) {
-  exec('yarn autotools snap', (err, stdout, stderr) => {
+  exec('yarn autotools-snap', (err, stdout, stderr) => {
     if (err) {
       // tslint:disable-next-line:no-console
       console.error(err);
+      process.exitCode = (err as any).status;
     }
 
     process.stdout.write(stdout);

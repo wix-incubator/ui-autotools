4c64d35a542ca2a57b6d624dd3e9ee7bbae59f86
Kieran Williams <kieranw@wix.com>
2018-12-11 13:55:29 +0200

    Trying to use Mocha logger

--- packages/mock-repo/snap-if-ci.ts
+++ packages/mock-repo/snap-if-ci.ts
@@ -3,6 +3,7 @@ import {exec} from 'child_process';
 
 if (isCI) {
   exec('yarn autotools-snap', (err, stdout, stderr) => {
+    console.log('SNAPPING ON CI')
     if (err) {
       process.exitCode = (err as any).status;
     }

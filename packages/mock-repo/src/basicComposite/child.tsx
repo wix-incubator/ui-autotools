f73e989069cfd35769125e2923d731ddb589e5fd
Daniel Strokovsky <danielstr@wix.com>
2019-01-13 11:27:38 +0200

    removed files

--- packages/mock-repo/src/basicComposite/child.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-export interface IProps {
-  text: React.ReactNode;
-  className?: string;
-}
-
-export const ChildComp: React.FunctionComponent<IProps> = (props) =>
-  <p>❦ {props.text} ❦</p>;
